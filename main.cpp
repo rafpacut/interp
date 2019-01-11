5497466b542c9a35bd46b703b7324bd0ce49cbda
rafal <rafpacut@gmail.com>
2019-01-11 10:20:46 +0100

    Moved parse to it's own function

--- main.cpp
+++ main.cpp
@@ -1,38 +1,69 @@
-#include <iostream>
-#include <iomanip>
-#include <iterator>
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/home/x3.hpp>
+#include <boost/spirit/home/x3/support/ast/position_tagged.hpp>
+#include <boost/spirit/home/x3/support/utility/error_reporting.hpp>
+#include <boost/spirit/home/x3/support/utility/annotate_on_success.hpp>
+
+#include <list>
 #include <algorithm>
 #include <fstream>
 #include <string>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
 
 #include "ast.h"
 #include "astAdapted.hpp"
+#include "grammar.cpp"
 #include "cmdArgumentProcessor.cpp"
 #include "env.hpp"
 #include "astPrinter.h"
 #include "eval.h"
-#include "grammar.cpp"
 
-namespace x3 = boost::spirit::x3;
-using namespace x3;
+std::list<ast::Statement>
+parse(std::string const& input)
+{
+    using boost::spirit::x3::ascii::space;
+    using boost::spirit::x3::eol;
 
-int main(int argc, char **argv) 
+    typedef std::string::const_iterator iterator_type;
+    iterator_type iter = input.begin();
+    iterator_type const end = input.end();
+
+    std::list<ast::Statement> ast;
+    auto const parser = grammar::program;
+    bool r = phrase_parse(iter, end, parser, (eol|space), ast);
+
+    if (r && iter == end)
+    {
+            std::cout << "-------------------------\n";
+            std::cout << "Parsing succeeded\n";
+            std::cout << "-------------------------\n";
+    }
+    else
+    {
+        std::cout << "-------------------------\n";
+        std::cout << "Parsing failed\n";
+        std::cout << "-------------------------\n";
+        ast.clear();
+    }
+    return ast;
+}
+
+int
+main(int argc, char** argv)
 {
 	auto [filePath, runInteractive, debug] = cmdArgProcessor::process(argc, argv);
 	
 	//if provided an incorrect filePath run interactive
+	//Why not in cmdArgProcessor?
 	if(!filePath)
 		runInteractive = true;
 
-	auto &parser = grammar::program;
-	std::list<ast::Statement> ast; 
-	ast::Printer print;
-	ast::Eval eval(debug);
-	
-	using It = std::string::const_iterator;
+       ast::Printer print;
+       ast::Eval eval(debug);
+
 	std::string str;
-
-
 	if(!runInteractive) 
 	{
 	       //read the file
@@ -40,40 +71,31 @@ int main(int argc, char **argv)
 	       str = std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
 	
 	       //process
-	       It iter = str.begin(), end = str.end();
-	       if(phrase_parse(iter, end, parser, (x3::space| x3::eol), ast))
+	       auto ast = parse(str);
+	       if(!ast.empty())
 	       {
-	    	if(debug)
-	    	{	
-	    		std::cout<< "Parsing succeded\n";
-	    		print(ast);
-	    	}
-	
-	    	try
-	    	{
-	    		eval(ast);
-	    	}
-	    	catch(std::runtime_error& e)
-	    	{
-	    		std::cout<<e.what()<<std::endl;
-	    		return 1;
-	    	}
-	    	catch(std::out_of_range& e) //doesn't say which var is undeclared.
-	    	{
-	    		std::cout<<"Using undeclared variable"<<std::endl;
-	    	}
-	    	catch(...)
-	    	{
-			std::cout<<"Unknown error happened\n";
-	    		return 1;
-	    	}
-	
-	    	if (iter != end)
-	    		std::cout << "Remaining unparsed: " << std::quoted(std::string(iter, end), '\'') << "\n";
+		       if(debug)
+			       print(ast);
+
+		    	try
+		    	{
+		    		eval(ast);
+		    	}
+		    	catch(std::runtime_error& e)
+		    	{
+		    		std::cout<<e.what()<<std::endl;
+		    		return 1;
+		    	}
+		    	catch(std::out_of_range& e) //doesn't say which var is undeclared.
+		    	{
+		    		std::cout<<"Using undeclared variable"<<std::endl;
+		    	}
+		    	catch(...)
+		    	{
+				std::cout<<"Unknown error happened\n";
+		    		return 1;
+		    	}
 	       }
-	       else
-	        std::cout << "Parsing failed\n";
-	
 	}
 	else
 	{
@@ -82,42 +104,35 @@ int main(int argc, char **argv)
 	
 	        while (std::getline(std::cin, str)) 
 	        {
-	    	if (str[0] == 'q' || str[0] == 'Q')
-	    	    break;
-	    	if(str.empty())
-	    	       	continue;
-	
-	    	It iter = str.begin(), end = str.end();
-	    	if(phrase_parse(iter, end, parser, (x3::space| x3::eol), ast))
-	    	{
-	    		if(debug)
-	    		{
-	    		    std::cout << "Parsing succeeded\n";
-	    		    print(ast);
+	    		if (str[0] == 'q' || str[0] == 'Q')
+	    		    break;
+	    		if(str.empty())
+	    		       	continue;
+
+			auto ast = parse(str);
+			if(!ast.empty())
+			{
+			       if(debug)
+				       print(ast);
+	    		    try
+	    		    {
+	    			   eval(ast);
+	    		    }
+	    		    catch(std::runtime_error& e)
+	    		    {
+	    			    std::cout<<e.what()<<std::endl;
+	    		    }
+	        	        catch(std::out_of_range& e)
+	    		    {
+	    		            std::cout<<"Using undeclared variable"<<std::endl;
+	    		    }
+	    		    catch(...)
+	    		    {
+					std::cout<<"Unknown error happened\n";
+	    		    }//do nothing. Rn all throwable exceptions allow processing of the next query.
 	    		}
-	
-	    	    try
-	    	    {
-	    		   eval(ast);
-	    	    }
-	    	    catch(std::runtime_error& e)
-	    	    {
-	    		    std::cout<<e.what()<<std::endl;
-	    	    }
-	                catch(std::out_of_range& e)
-	    	    {
-	    	            std::cout<<"Using undeclared variable"<<std::endl;
-	    	    }
-	    	    catch(...)
-	    	    {
-				std::cout<<"Unknown error happened\n";
-	    	    }//do nothing. Rn all throwable exceptions allow processing of the next query.
-	    	}
-	    	else
-	    	    std::cout << "Parsing failed\n";
-	
-	    	if (iter != end)
-	    	    std::cout << "Remaining unparsed: " << std::quoted(std::string(iter, end), '\'') << "\n";
 	        }
 	}
+
+    return 0;
 }
